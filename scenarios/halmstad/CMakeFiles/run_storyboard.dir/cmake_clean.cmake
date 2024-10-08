22c276aa3afbfc801e1ca57af1a7b32b92815055
3gles00 <haron.obaid66@gmail.com>
2024-10-08 17:18:49 +0200

    updates setting up artery and git

--- scenarios/halmstad/CMakeFiles/run_storyboard.dir/cmake_clean.cmake
+++ scenarios/halmstad/CMakeFiles/run_storyboard.dir/cmake_clean.cmake
@@ -1,8 +1,8 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/run_storyboard"
+  "CMakeFiles/run_halmstad"
 )
 
 # Per-language clean rules from dependency scanning.
 foreach(lang )
-  include(CMakeFiles/run_storyboard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+  include(CMakeFiles/run_halmstad.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
